3061e6df6320daf2e07a96f61bafdbcc87b005af
Pablerdo <pablosalamanca88@gmail.com>
2025-02-11 20:08:19 -0500

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css"
 import type React from "react"
 
 export const metadata = {
-  title: "Hexaframe",
-  description: "Generate videos with AI",
+  title: "Hexaframe Demo",
+  description: "Generate controllable videos with AI",
 }
 
 export default function RootLayout({
